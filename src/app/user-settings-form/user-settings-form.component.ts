66912d176aa504ef4262f065dac7492c5adb1e2e
Inventrohyder <22187034+Inventrohyder@users.noreply.github.com>
2020-01-21 14:59:28 +0530

    lesson: Copying Form Data

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -8,13 +8,18 @@ import { UserSettings } from '../data/user-settings';
 })
 export class UserSettingsFormComponent implements OnInit {
 
-  userSettings: UserSettings = {
+  originalUserSettings: UserSettings = {
     name: 'Milton', 
     emailoffers: true,
     interfaceStyle: 'dark',
     subscriptionType: 'Annual',
     notes: 'here are some notes...'
   }
+
+  // Using a copy of the original user settings to avoid data corruption
+  // in case the user cancels the form or hits the back button without 
+  // completing the form
+  userSettings : UserSettings = { ...this.originalUserSettings };
 
   constructor() { }
 
